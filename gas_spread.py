693160650e96cfa3acf16335c0a1ab98e1eeafdd
Nikolay Simakov <n.simakoff@gmail.com>
2023-09-30 19:37:46 +0300

    Gas spread module

---
+++ gas_spread.py
@@ -0,0 +1,32 @@
+from typing import List
+from models import Gas, EvacuationMap
+import time
+
+
+def update_gas_filling(gas: Gas, evaluation_map: EvacuationMap, num_iters: int):
+    
+    def pprint(mat):
+        for i in mat:
+            print(i)
+    
+    def get_neighbours(x, y):
+        res = []
+        for xx, yy in [(x+1, y), (x-1, y), (x, y+1), (x, y-1)]:
+            if 0 <= xx < len(evaluation_map.ev_map[0]) and 0 <= yy < len(evaluation_map.ev_map) and evaluation_map.ev_map[yy][xx] == 0:
+                res.append((xx, yy))
+        return res
+    
+    queue = [(gas.pos.x, gas.pos.y)]
+    
+    for _ in range(num_iters):
+        for _ in range(len(queue)):
+            first_el = queue.pop(0)
+            evaluation_map.ev_map[first_el[1]][first_el[0]] = 3
+            queue += get_neighbours(first_el[0], first_el[1])
+        
+        # print(evaluation_map.ev_map)
+        pprint(evaluation_map.ev_map)
+        print()
+        time.sleep(1.0)
+
+    
